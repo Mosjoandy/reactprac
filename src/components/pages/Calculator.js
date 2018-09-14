fa91bf2f554b16d4feef501eb6f38decf4db4305
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-09-14 14:40:03 -0700

    attempt to set operator input as a real operator versus just a string

--- src/components/pages/Calculator.js
+++ src/components/pages/Calculator.js
@@ -5,42 +5,63 @@ class Calculator extends React.Component {
     super()
     this.state = {
       value1: "",
+      operator: "",
       value2: "",
-      operator: ""
-    }
+      answer: ""
+    };
 
     this.handleChange1 = this.handleChange1.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-    // this.currentValues = this.currentValues.bind(this);
-  }
-
-  currentValues() {
-    console.log("value1 = " + this.state.value1 + "\nvalue2 = " + this.state.value2 + "\noperator = " + this.state.operator)
-
-  }
+    this.handleChange2 = this.handleChange2.bind(this);
+    this.handleChange3 = this.handleChange3.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
 
   handleChange1(event) {
-    this.setState({ value1: event.target.value })
-  }
+    this.setState({ value1: event.target.value });
+  };
+
+  handleChange2(event) {
+    this.setState({ value2: event.target.value });
+  };
+
+  handleChange3(event) {
+    this.setState({ operator: event.target.value });
+  };
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log("submitted")
-  }
+    var answer = parseInt(this.state.value1, 10) + parseInt(this.state.value2, 10);
+    console.log(answer);
+    this.setState({answer: answer});
+    console.log("Calculate you MONSTER");
+    console.log("value1 = " + this.state.value1 + "\noperator = " + this.state.operator + "\nvalue2 = " + this.state.value2);
+  };
 
   render() {
     return (
       <div>
-        <button tpye="button" onClick={() => this.currentValues()}>Current Value Test</button>
+        {/* <button tpye="button" onClick={() => this.currentValues()}>Current Value Test</button> */}
         <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.value1} onChange={this.handleChange1} />
-          {/* <input type="text" value={this.state.operator} onChange={this.handleChange} />
-          <input type="text" value={this.state.value2} onChange={this.handleChange} /> */}
+
+          <label>First Number</label>
+          <input type="number" value={this.state.value1} onChange={this.handleChange1} />
+          <br />
+
+          <label>Operator</label>
+          <input type="text" maxLength="1" size="1" value={this.state.operator} onChange={this.handleChange3} />
+          <br />
+
+          <label>Second Number</label>
+          <input type="number" value={this.state.value2} onChange={this.handleChange2} />
+          <br />
+
           <input type="submit" value="Submit" />
+
         </form>
+        <div>{this.state.value1} {this.state.operator} {this.state.value2} = {this.state.answer}</div>
       </div>
-    )
-  }
+    );
+  };
 };
 
 export default Calculator;
