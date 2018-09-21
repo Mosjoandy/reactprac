3c8874f14357106356e7963dbb31a522bc6f858e
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-09-20 18:38:21 -0700

    adding comments for thought process

--- src/components/pages/Calculator.js
+++ src/components/pages/Calculator.js
@@ -18,6 +18,7 @@ class Calculator extends React.Component {
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
+    // using [event.target.name] allows the use of multiple input fields, as long as "name='thing'" is specified in the input
   };
 
   // handleChange2(event) {
@@ -27,6 +28,7 @@ class Calculator extends React.Component {
   // handleChange3(event) {
   //   this.setState({ operator: event.target.value });
   // };
+  // ^^^^^^ ABANDONED - consolidate handleChange functions
 
   handleSubmit(event) {
     event.preventDefault();
@@ -46,15 +48,25 @@ class Calculator extends React.Component {
     //   this.setState({ answer: parseInt(this.state.value1, 10) * parseInt(this.state.value2, 10)}) 
 
     // } else this.setState({ answer: "Pick these operators: +, -, /, *"})
+    // ^^^^^^ ABANDONED - switch is a better choice for this kind of operation
 
     const operator = this.state.operator;
     const value1 = parseInt(this.state.value1, 10);
     const value2 = parseInt(this.state.value2, 10);
+    // setting values from input fields to variables, then parsing those strings into numbers with parseInt(#, hex value)
 
     switch (operator) {
+    // switch case passing variable operator (because operator is what the cases will be)
+
       case "+":
+      // Takes the string from operator input form and run the appropriate state change
+
         this.setState({ answer: value1 + value2 });
+        // sets new state of answer based on switch case value running addition in this line
+
         break
+        // ends the switch if this case is matched
+
       case "-":
         this.setState({ answer: value1 - value2 });
         break
@@ -65,18 +77,22 @@ class Calculator extends React.Component {
         this.setState({ answer: value1 * value2 });
         break
       default:
-        console.log("Pick these operators: +, -, /, *");
+      // if none of the cases are matched, run this instead:
+
+        // console.log("Pick these operators: +, -, /, *");
         this.setState({ answer: "Pick these operators: +, -, /, *" });
+        // Telling users that they need to pick a real operator
     };
 
     // var answer = (parseInt(this.state.value1, 10) + parseInt(this.state.value2, 10))
-    // console.log(this.state.value1 + "+" + this.state.value2+"="+answer);
+    // ^^^^^^ ABANDONED - original parsing of answer prior to switch
   };
 
   render() {
     return (
       <div>
         {/* <button tpye="button" onClick={() => this.currentValues()}>Current Value Test</button> */}
+        {/* ^^^^^^ ABANDONED - showing values of operators without needing to open consolelog */}
         <form onSubmit={this.handleSubmit}>
 
           <label>First Number</label>
@@ -95,6 +111,7 @@ class Calculator extends React.Component {
 
         </form>
         <div>{this.state.value1} {this.state.operator} {this.state.value2} = {this.state.answer}</div>
+        {/* rendering the values and the operator to show user the answer */}
       </div>
     );
   };
